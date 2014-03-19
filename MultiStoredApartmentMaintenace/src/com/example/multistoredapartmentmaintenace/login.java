0181cc779d63ff20fb6152ae236ac7d7433531ab
suraj5 <kmit.suraj@gmail.com>
2014-03-20 02:25:44 +0530

    MultiStoredApartmentApp-1

---
+++ MultiStoredApartmentMaintenace/src/com/example/multistoredapartmentmaintenace/login.java
@@ -0,0 +1,24 @@
+package com.example.multistoredapartmentmaintenace;
+
+public class login {
+	private String username;
+	private String password;
+	
+	public String getuser()
+	{
+		return username;
+	}
+	public String getpassword(){
+		return password;
+		
+	}
+	public void setUsername(String username)
+	{
+		this.username=username;
+	}
+	public void setPassword(String password)
+	{
+		this.password=password;
+	}
+
+}
